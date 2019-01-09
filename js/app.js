68de83d3728b62c00d6d9382e8a9aa0312b36661
fabianburghardt <fabsiburghardt@yahoo.de>
2019-01-09 16:57:26 +0100

    cleaned up

--- js/app.js
+++ js/app.js
@@ -1,30 +1,13 @@
-function initialize(){
-  ARSynth.init({
-    noteElm: $('#note')[0],
-    frequencyElm: $('#frequency')[0]
-  });
-
-  $('#color').bind('change', function(){
-    ARSynth.set('color', this.value); // hex color
-  });
-
-  $('#color-offset').bind('change', function(){
-    ARSynth.set('cOffset', parseInt(this.value, 10));
-  });
-
-  $('#size-offset').bind('change', function(){
-    ARSynth.set('sizeOffset', parseInt(this.value, 10));
-  });
-}
-
-var synth = new Tone.Synth().toMaster();
-
-// //play a middle 'C' for the duration of an 8th note
+function initialize(){ARSynth.init({noteElm: $('#note')[0], frequencyElm: $('#frequency')[0] }); $('#color').bind('change', function(){ARSynth.set('color', this.value); // hex color }); $('#color-offset').bind('change', function(){ARSynth.set('cOffset', parseInt(this.value, 10)); }); $('#size-offset').bind('change', function(){ARSynth.set('sizeOffset', parseInt(this.value, 10)); }); }
+// var synth = new Tone.Synth().toMaster();
 // synth.triggerAttackRelease("C#3", "20n");
 
 
 
 
+/* Hier die Farbe zum Tracken eintragen, wenn ihr sie Fix machen wollt */
+
+ARSynth.set('color', "#780011"); //
 
 /* Euer Code */
 
@@ -33,14 +16,11 @@ function checkIfColorVisible(){
   // Hier wird der Text auf den Screen geschrieben, ob sichtbar oder nicht
   $("#debugger").text("farbe sichtbar: " + farbe1);
 
-
   // Hier schauen wir if-else mäßig, ob Farbe1 sichtbar ist oder nicht
   if(farbe1 == true){
-    // Ist sichtbar
-    fadeInSound1();
+    fadeInSound1(); // Ist sichtbar
   }else{
-    // Ist nicht sichtbar
-    fadeOutSound1();
+    fadeOutSound1(); // Ist nicht sichtbar
   }
 
 
@@ -50,12 +30,11 @@ function checkIfColorVisible(){
   },100)
 
 }
-
 checkIfColorVisible();
 
 
 
-
+/* Hier passieren dann die Audio sachen */
 function fadeInSound1(){
 
 }
